6aafe4e100a73d515fc298b15dfd15317709005d
Satyam Gupta <satyamgupta9302@gmail.com>
2026-02-15 16:03:11 +0530

    add repository schema model

---
+++ backend/models/repoModel.js
@@ -0,0 +1,37 @@
+import mongoose, { Schema } from "mongoose";
+
+const RepositorySchema = new Schema({
+    timestamps: true,
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    description: {
+        type: String,
+    },
+    content: [
+        {
+        type: String,
+        },
+    ],
+    visibility: {
+        type: Boolean,
+    },
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    issues: [
+        {
+        type: Schema.Types.ObjectId,
+        ref: "Issue",
+        },
+    ],
+});
+
+const Repository = mongoose.model("Repository", RepositorySchema);
+
+
+export default Repository;
