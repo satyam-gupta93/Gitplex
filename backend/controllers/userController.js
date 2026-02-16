595dd97a3ca80bb1c254cc88f0aabd1960055095
Satyam Gupta <satyamgupta9302@gmail.com>
2026-02-16 17:27:59 +0530

    mplement user login functionality

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -27,9 +27,9 @@ const signup = async (req, res) => {
         await newUser.save();
 
         const token = jwt.sign(
-        { id: newUser._id },
-        process.env.JWT_SECRET_KEY,
-        { expiresIn: "1h" }
+            { id: newUser._id },
+            process.env.JWT_SECRET_KEY,
+            { expiresIn: "1h" }
         );
 
         res.json({ token, userId: newUser._id });
@@ -38,6 +38,36 @@ const signup = async (req, res) => {
         res.status(500).send("Server error");
     }
 };
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+    try {
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(400).json({ message: "Invalid credentials!" });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return res.status(400).json({ message: "Invalid credentials!" });
+        }
+
+        const token = jwt.sign(
+            { id: user._id },
+            process.env.JWT_SECRET_KEY,
+            { expiresIn: "1h" }
+        );
+
+        res.json({ token, userId: user._id });
+    } catch (err) {
+        console.error("Error during login:", err.message);
+        res.status(500).send("Server error!");
+    }
+};
+
 
 
 
