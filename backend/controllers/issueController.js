097671508ef6431c8ea7a9a966091f0692a7534b
Satyam Gupta <satyamgupta9302@gmail.com>
2026-02-17 18:11:01 +0530

    Implement createIssue functionality

--- backend/controllers/issueController.js
+++ backend/controllers/issueController.js
@@ -1,6 +1,27 @@
+import mongoose from "mongoose";
+import Repository from "../models/repoModel.js";
+import User from "../models/userModel.js";
+import Issue from "../models/issueModel.js";
 
-const createIssue = (req,res) =>{
-    res.send("Issue created!");
+
+const createIssue = async (req,res) =>{
+    const { title, description } = req.body;
+    const { id } = req.params;
+
+    try {
+        const issue = new Issue({
+        title,
+        description,
+        repository: id,
+        });
+
+    await issue.save();
+
+    res.status(201).json(issue);
+    } catch (err) {
+        console.error("Error during issue creation : ", err.message);
+        res.status(500).send("Server error");
+    }
 }
 
 const updateIssueById = (req,res) =>{
