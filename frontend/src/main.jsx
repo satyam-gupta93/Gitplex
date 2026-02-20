755564984de94c20a2d376cebb4038a610e9f5aa
Satyam Gupta <satyamgupta9302@gmail.com>
2026-02-20 16:34:29 +0530

    Implement routes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { AuthProvider } from './authContext.jsx'
+import ProjectRoutes from './Routes.jsx';
+import { BrowserRouter as Router } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
- <AuthProvider>
-    <App />
- </AuthProvider>
-    
+  <AuthProvider>
+    <Router>
+      <ProjectRoutes />
+    </Router>
+  </AuthProvider>
   
 )
